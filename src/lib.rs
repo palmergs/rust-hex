c18bf02efce003e8f18ab79d4d20b2bf7b0b9470
Galen Palmer <palmergs@gmail.com>
2020-05-29 20:23:28 -0400

    add pub method

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
 #[macro_use]
 extern crate lazy_static;
 
+use wasm_bindgen::prelude::*;
+
 mod axial;
 mod cube;
+
+#[wasm_bindgen]
+pub fn weasel() -> String {
+  "there are so many weasels here".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::weasel;
+
+  #[test]
+  fn test_weasel() {
+    assert_eq!(weasel(), "there are so many weasels here")
+  }
+}
