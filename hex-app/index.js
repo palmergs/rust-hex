cb5ff141b9b053a6305c6d51386d202de635be30
Galen <palmergs@gmail.com>
2020-06-13 15:13:37 -0400

    :construction: window.load still not working firefox

--- hex-app/index.js
+++ hex-app/index.js
@@ -1,7 +1,5 @@
 import * as wasm from "rust-hex";
 
-try {
-
 const resize = function() {
   console.log("resizing canvas...");
   let can = document.getElementById("canvas");
@@ -10,23 +8,18 @@ const resize = function() {
   console.log("...canvas resized: ", can.width, can.height);
 };
 
-
 window.addEventListener('load', (evt) => {
   console.log("in load..."+ evt);
   resize();
   console.log(wasm.draw_hexes());
   console.log(wasm.weasel());
   wasm.start_loop();
-});
+}, false);
 
 window.addEventListener('resize', (evt) => {
   console.log("in resize...");
   resize();
   console.log(wasm.draw_hexes());
   console.log(wasm.weasel());
-});
+}, false);
 
-} catch(exc) {
-  console.log("THERE WAS AN EXCEPTION DID YOU MISS IT?");
-  console.log(exc);
-}
