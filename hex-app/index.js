87c9fc68fa1926bc5d6ef80c57ff732fca258bf4
Galen <palmergs@gmail.com>
2020-06-13 15:05:00 -0400

    :construction: window.load is not working in firefox linux for me

--- hex-app/index.js
+++ hex-app/index.js
@@ -1,22 +1,32 @@
 import * as wasm from "rust-hex";
 
-function resizeCanvas() {
+try {
+
+const resize = function() {
   console.log("resizing canvas...");
   let can = document.getElementById("canvas");
-  can.width = document.body.clientWidth;
-  // can.height = document.body.clientHeight;
+  can.width = window.innerWidth - 18;
+  can.height = window.innerHeight - 100;
   console.log("...canvas resized: ", can.width, can.height);
 };
 
-window.onload = () => {
-  resizeCanvas();
-  console.log(wasm.weasel());
-  console.log(wasm.draw_hexes());
-  wasm.start_loop();
-}
 
-window.onresize = () => {
-  resizeCanvas();
-  console.log(wasm.weasel());
+window.addEventListener('load', (evt) => {
+  console.log("in load..."+ evt);
+  resize();
   console.log(wasm.draw_hexes());
+  console.log(wasm.weasel());
+  wasm.start_loop();
+});
+
+window.addEventListener('resize', (evt) => {
+  console.log("in resize...");
+  resize();
+  console.log(wasm.draw_hexes());
+  console.log(wasm.weasel());
+});
+
+} catch(exc) {
+  console.log("THERE WAS AN EXCEPTION DID YOU MISS IT?");
+  console.log(exc);
 }
